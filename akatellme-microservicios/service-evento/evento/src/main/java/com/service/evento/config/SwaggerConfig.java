b388df6942925d4ba21007f13c8551253ea30019
Ian Muñoz <iaa.munozc@duocuc.cl>
2026-06-19 19:06:33 -0400

    actualizacion de eventos y notificaciones

--- akatellme-microservicios/service-evento/evento/src/main/java/com/service/evento/config/SwaggerConfig.java
+++ akatellme-microservicios/service-evento/evento/src/main/java/com/service/evento/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Microservicio eventos")
+                        .title("API Microservicio Eventos")
                         .version("1.0.0")
                         .description("Documentación del microservicio encargado de gestionar eventos.")
                         .contact(new Contact()
